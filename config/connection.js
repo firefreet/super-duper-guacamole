3db853ab181b9f0502f3a6a2ec4d906bc22e9b08
Jeremy Marotta <firefreet@gmail.com>
2020-02-23 21:03:21 -0500

    update db connection to use jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql')
 const password = require('./password')
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: password,
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: password,
+        database: "burgers_db"
+    });
+}
 module.exports = connection
